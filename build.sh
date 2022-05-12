a946e0b3b8582fbf301e294aea89182890a2d638
Charles Tabor <ct37@txstate.edu>
2022-05-12 17:15:44 -0500

    simplify build script

--- build.sh
+++ build.sh
@@ -3,5 +3,4 @@ DEPLOY_DIR=${1:-local_modules}
 mkdir -p "${DEPLOY_DIR}"
 rm -rf "$DEPLOY_DIR"/queryscope-*.tgz dist/
 npm run build && npm pack --pack-destination ./
-mkdir -p "$DEPLOY_DIR/"
-mv queryscope-*.tgz  "$DEPLOY_DIR/"
+mv queryscope-*.tgz "$DEPLOY_DIR/"
