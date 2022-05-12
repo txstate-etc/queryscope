43d1ffb70abd222cc477c0e274d2b935bca74301
Charles Tabor <ct37@txstate.edu>
2022-05-12 17:18:18 -0500

    First attempt to move tokenizer transformer to view project as a program level

--- transformers/tokenizer.ts
+++ transformers/tokenizer.ts
@@ -1,4 +1,4 @@
-import * as ts from 'typescript'
+import ts from 'typescript'
 import { createHmac } from 'crypto'
 import { sign } from 'jsonwebtoken'
 
@@ -27,114 +27,124 @@ enum QSState {
   QSObjectLiteralExpression = 'QUERYSCOPE_OBJECT_LITERAL_EXPRESSION'
 }
 
-//const transformerProgram = (program: ts.Program) => {
+type TransformerOptions = {
+  privateKey?: string,
+  issuer?: string,
+  clientId?: string
+}
 
+export function transformer(program: ts.Program, opts?: TransformerOptions) {
   // Create array of found QueryScopeParts
   const foundQueryScopeParts = new Map<string, string>()
   let templateExpanded: string;
-  
-  const transformerFactory: ts.TransformerFactory<ts.SourceFile> = context => {
-    return sourceFile => {
-      let state: QSState = QSState.Start
-      let variable: string|undefined
-      let query = false
-      //   TemplateExpresson:
-      //     TemplateHead,
-      //     TemplateSpan(Identifier,TemplateMiddle),
-      //     TemplateSpan(Identifier,TemplateTail)
-      const templateExpander = (node: ts.Node): ts.Node => {
-        if (ts.isStringLiteral(node) || ts.isNoSubstitutionTemplateLiteral(node)) {
-          templateExpanded = node.getText().replace(/(^["'`])|(["'`]$)/g, '')
-          if (!query) foundQueryScopeParts.set(variable || 'unknown', templateExpanded)
-          console.log('  templateExpander %s = %s', variable, JSON.stringify(templateExpanded))
-          return node
-        } else if (ts.isTemplateHead(node) || ts.isTemplateMiddle(node)) {
-          templateExpanded += node.text
-        } else if (ts.isIdentifier(node)) {
-          if (foundQueryScopeParts.has(node.getText())) {
-            templateExpanded += foundQueryScopeParts.get(node.getText())
-          } else {
-            throw Error('QueryScopePart '+node.getText()+' not found.')
-          }
-        } else if (ts.isTemplateTail(node)) {
-          templateExpanded += node.text
-          console.log('  templateExpander - tail: %s', JSON.stringify(templateExpanded))
-          if (!query) foundQueryScopeParts.set(variable || 'unknown', templateExpanded)
-        }
-        return ts.visitEachChild(node, templateExpander, context)
-      };
-      const startTemplateExpander = (node: ts.Node): ts.Node => {
-        templateExpanded = ''
-        console.log('Start template expander: %s', variable)
-        return templateExpander(node)
-      };
-      const queryScopeObject = (node: ts.Node): ts.Node => {
-        if (ts.isIdentifier(node) && node.getText() === 'query') {
-          query = true
-        } else if (query && (ts.isStringLiteral(node) || ts.isNoSubstitutionTemplateLiteral(node) || ts.isTemplateExpression(node) ) ) {
-          startTemplateExpander(node)
-          return node
-        }
-        return ts.visitEachChild(node, queryScopeObject, context)
-      };
-      const startQueryScopeObject = (node: ts.Node): ts.Node => {
-        // There is an invalid case where TypeScript would produce an error
-        // during compilation, should no query property is found.
-        // Check if token mataches query and if not then return with all properties with new token value.
-        // Probably will not implement this as tokens will never exist in the ts file as they are generated
-        // at build time and will only exist within the image. We are going just overwrite the token.
-        // Check declorator to see if we wish to maintain this query and if so then return
-        // all properties with an appended token property and the generated token value
-        const newNode = queryScopeObject(node)
-        query = false
-        const token = syncSignQueryDigest(templateExpanded)
-        // console.log("Found variable %s, with only query: %s, and new token %s", variableId, JSON.stringify(query), token)
-        if (token != null) {
-          return ts.factory.createObjectLiteralExpression([
-            ts.factory.createPropertyAssignment('query', ts.factory.createStringLiteral(templateExpanded)),
-            ts.factory.createPropertyAssignment('token', ts.factory.createStringLiteral(token))
-          ])
-        } else {
-          return newNode
-        }
-      };
-      const visitor = (node: ts.Node): ts.Node => {
-        if (QUERYSCOPE_CLIENT_ID == null || QUERYSCOPE_PRIVATE_KEY == null) {
-          console.log('WARN: No QUERYSCOPE private key cert or client id was found. Will skip token signing process.')
-          return node
-        }
-        console.log("Signing client %s queries with %s issuer.", QUERYSCOPE_CLIENT_ID, QUERYSCOPE_ISSUER)
-        if (ts.isVariableDeclaration(node)) {
-          state = QSState.VariableDeclaration
-          variable = undefined
-        } else if (state === QSState.VariableDeclaration && ts.isIdentifier(node)) {
-          variable = node.getText()
-        } else if (state === QSState.VariableDeclaration && ts.isTypeReferenceNode(node)) {
-          if (node.getText() === 'QueryScopePart') {
-            state = QSState.QSPTypeReference
-            console.log('QueryScopePart variable declaration: %s', variable)
-          } else if (node.getText() === 'QueryScope') {
-            state = QSState.QSTypeReference
-            console.log('QueryScope variable declaration: %s', variable)
-          } else {
-            state = QSState.Start
-          }
-        } else if (state === QSState.QSPTypeReference) {
-          if (ts.isStringLiteral(node) || ts.isNoSubstitutionTemplateLiteral(node) || ts.isTemplateExpression(node)) {
-            state = QSState.Start
-            return startTemplateExpander(node)
-          }
-        } else if (state === QSState.QSTypeReference && ts.isObjectLiteralExpression(node)) {
-          state = QSState.Start
-          return startQueryScopeObject(node)
-        }
-        return ts.visitEachChild(node, visitor, context)
-      };
-      return ts.visitNode(sourceFile, visitor)
-    };
-  };
-//  return transformerFactory;
-//};
 
-export default transformerFactory;
-//export default transformerProgram;
+  function visitor(ctx: ts.TransformationContext, sf: ts.SourceFile) {
+    const typeChecker = program.getTypeChecker();
+
+    let state: QSState = QSState.Start
+    let variable: string|undefined
+    let query = false
+    //   TemplateExpresson:
+    //     TemplateHead,
+    //     TemplateSpan(Identifier,TemplateMiddle),
+    //     TemplateSpan(Identifier,TemplateTail)
+    const templateExpander = (node: ts.Node): ts.Node => {
+      if (ts.isStringLiteral(node) || ts.isNoSubstitutionTemplateLiteral(node)) {
+        templateExpanded = node.getText().replace(/(^["'`])|(["'`]$)/g, '')
+        if (!query) foundQueryScopeParts.set(variable || 'unknown', templateExpanded)
+        console.log('  templateExpander %s = %s', variable, JSON.stringify(templateExpanded))
+        return node
+      } else if (ts.isTemplateHead(node) || ts.isTemplateMiddle(node)) {
+        templateExpanded += node.text
+      } else if (ts.isIdentifier(node)) {
+        if (foundQueryScopeParts.has(node.getText())) {
+          templateExpanded += foundQueryScopeParts.get(node.getText())
+        } else {
+          throw Error('QueryScopePart '+node.getText()+' not found.')
+        }
+      } else if (ts.isTemplateTail(node)) {
+        templateExpanded += node.text
+        console.log('  templateExpander - tail: %s', JSON.stringify(templateExpanded))
+        if (!query) foundQueryScopeParts.set(variable || 'unknown', templateExpanded)
+      }
+      return ts.visitEachChild(node, templateExpander, ctx)
+    };
+    const startTemplateExpander = (node: ts.Node): ts.Node => {
+      templateExpanded = ''
+      console.log('Start template expander: %s', variable)
+      return templateExpander(node)
+    };
+    const queryScopeObject = (node: ts.Node): ts.Node => {
+      if (ts.isIdentifier(node) && node.getText() === 'query') {
+        query = true
+      } else if (query && (ts.isStringLiteral(node) || ts.isNoSubstitutionTemplateLiteral(node) || ts.isTemplateExpression(node) ) ) {
+        startTemplateExpander(node)
+        return node
+      }
+      return ts.visitEachChild(node, queryScopeObject, ctx)
+    };
+    const startQueryScopeObject = (node: ts.Node): ts.Node => {
+      // There is an invalid case where TypeScript would produce an error
+      // during compilation, should no query property is found.
+      // Check if token mataches query and if not then return with all properties with new token value.
+      // Probably will not implement this as tokens will never exist in the ts file as they are generated
+      // at build time and will only exist within the image. We are going just overwrite the token.
+      // Check declorator to see if we wish to maintain this query and if so then return
+      // all properties with an appended token property and the generated token value
+      const newNode = queryScopeObject(node)
+      query = false
+      const token = syncSignQueryDigest(templateExpanded)
+      // console.log("Found variable %s, with only query: %s, and new token %s", variableId, JSON.stringify(query), token)
+      if (token != null) {
+        return ts.factory.createObjectLiteralExpression([
+          ts.factory.createPropertyAssignment('query', ts.factory.createStringLiteral(templateExpanded)),
+          ts.factory.createPropertyAssignment('token', ts.factory.createStringLiteral(token))
+        ])
+      } else {
+        return newNode
+      }
+    };
+    const visitor = (node: ts.Node): ts.Node => {
+      if (QUERYSCOPE_CLIENT_ID == null || QUERYSCOPE_PRIVATE_KEY == null) {
+        console.log('WARN: No QUERYSCOPE private key cert or client id was found. Will skip token signing process.')
+        return node
+      }
+      console.log("Signing client %s queries with %s issuer.", QUERYSCOPE_CLIENT_ID, QUERYSCOPE_ISSUER)
+      if (ts.isVariableDeclaration(node)) {
+        state = QSState.VariableDeclaration
+        variable = undefined
+      } else if (state === QSState.VariableDeclaration && ts.isIdentifier(node)) {
+        variable = node.getText()
+      } else if (state === QSState.VariableDeclaration && ts.isTypeReferenceNode(node)) {
+        if (node.getText() === 'QueryScopePart') {
+          state = QSState.QSPTypeReference
+          console.log('QueryScopePart variable declaration: %s', variable)
+        } else if (node.getText() === 'QueryScope') {
+          state = QSState.QSTypeReference
+          console.log('QueryScope variable declaration: %s', variable)
+        } else {
+          state = QSState.Start
+        }
+      } else if (state === QSState.QSPTypeReference) {
+        if (ts.isStringLiteral(node) || ts.isNoSubstitutionTemplateLiteral(node) || ts.isTemplateExpression(node)) {
+          state = QSState.Start
+          return node
+          //return startTemplateExpander(node)
+        }
+      } else if (state === QSState.QSTypeReference && ts.isObjectLiteralExpression(node)) {
+        state = QSState.Start
+        return node
+        //return startQueryScopeObject(node)
+      }
+      return ts.visitEachChild(node, visitor, ctx)
+    };
+    return visitor
+  }
+
+  return (ctx: ts.TransformationContext) => {
+    console.log("Got to this point")
+    return (sf: ts.SourceFile) => ts.visitNode(sf, visitor(ctx, sf))
+  };
+}
+
+export default transformer;
