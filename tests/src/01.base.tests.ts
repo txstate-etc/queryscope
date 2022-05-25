30a3769493bac4015299381045226e9816a1fb66
Charles Tabor <ct37@txstate.edu>
2022-05-25 15:55:46 -0500

    Test for query scope part removal and non-const skipped

--- tests/src/01.base.tests.ts
+++ tests/src/01.base.tests.ts
@@ -2,6 +2,8 @@ import { expect } from 'chai'
 import { QueryScope, QueryScopePart } from '../../lib/index'
 
 // Parts
+const someString: string = '  2'
+let querypart1: QueryScopePart = '  1'
 const querypart2: QueryScopePart = '  2'
 const querypart3: QueryScopePart = `  3`
 
@@ -28,8 +30,20 @@ const query1result: string = `  1
 
 describe('basic tests', function () {
   it('should sign QueryScope type that has no token', async () => {
-    console.log('query1 = %s', JSON.stringify(query1))
     expect(query1.query).to.be.a('string').equal(query1result)
     expect(query1.token).to.be.a('string')
+  })
+  it('should not remove (nor use) non const query scope parts', async () => {
+    expect(typeof querypart1).to.eq('string')
+    expect(querypart1).to.eq('  1')
+    // NOTE Cannot test if querypart1 is used as part of a QueryScope that an
+    // error will throw because error happens in build phase and not during testing.
+  })
+  it('should remove const query scope parts', async () => {
+    // Make sure can see other strings first
+    expect(typeof someString).to.eq('string')
+    expect(typeof querypart2).to.eq('undefined')
+    expect(typeof querypart3).to.eq('undefined')
+    expect(typeof querypart4).to.eq('undefined')
   })
 })
